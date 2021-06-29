4fabb2089943bc79736001996e7a767970619319
valeriyagracheva <valeria.gracheva@altenar.com>
2021-06-29 20:15:09 +0300

    Commit to save logo.svg

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,50 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">
+        <img src="./assets/image/logo.svg" alt="logo" width="50px" height="50px"> 
+      </router-link>
+      <ul>
+        <li>
+          <router-link to="/">Home</router-link>
+        </li>
+        <li>
+          <router-link to="/about">About</router-link>
+        </li>
+      </ul>
+      <router-link to="/">
+        <img src="./assets/image/buy.svg" alt="logo">
+      </router-link>
     </div>
     <router-view />
   </div>
 </template>
 
 <style lang="less">
+@font-face {
+  font-family: Open Sans;
+  src: url(https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap);
+}
+* {
+  box-sizing: border-box;
+}
+body {
+  background-color: #f5f6f7;
+  margin: 0;
+  padding: 25px;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Open Sans, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #141010;
 }
 
 #nav {
-  padding: 30px;
-
+  padding: 25px;
+  height: 100px;
+  display: flex;
   a {
     font-weight: bold;
     color: #2c3e50;
